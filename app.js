d167b1c54f10f78decc29a2832e509b69b4e7831
Rafael Guasp <ralfyguasp@gmail.com>
2018-08-24 00:08:05 -0400

    working 3d carousel

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-var carousel = $(".carousel"),
-    currdeg  = 0;
+$(document).ready(function(){  
+  var carousel = $(".carousel"),
+      currdeg  = 0;
 
-$(".next").on("click", { d: "n" }, rotate);
-$(".prev").on("click", { d: "p" }, rotate);
+  $(".next").on("click", { d: "n" }, rotate);
+  $(".prev").on("click", { d: "p" }, rotate);
 
-function rotate(e){
-  if(e.data.d=="n"){
-    currdeg = currdeg - 60;
+  function rotate(e){
+    if(e.data.d=="n"){
+      currdeg = currdeg - 60;
+    }
+    if(e.data.d=="p"){
+      currdeg = currdeg + 60;
+    }
+    carousel.css({
+      "-webkit-transform": "rotateY("+currdeg+"deg)",
+      "-moz-transform": "rotateY("+currdeg+"deg)",
+      "-o-transform": "rotateY("+currdeg+"deg)",
+      "transform": "rotateY("+currdeg+"deg)"
+    });
   }
-  if(e.data.d=="p"){
-    currdeg = currdeg + 60;
-  }
-  carousel.css({
-    "-webkit-transform": "rotateY("+currdeg+"deg)",
-    "-moz-transform": "rotateY("+currdeg+"deg)",
-    "-o-transform": "rotateY("+currdeg+"deg)",
-    "transform": "rotateY("+currdeg+"deg)"
-  });
-}
+})
